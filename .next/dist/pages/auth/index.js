b93389729b5171559c42b2e80a8353011fc1639a
Bill Buchanan <bbuchanan@gmail.com>
2018-01-10 16:12:58 -0500

    using 'normal' react components in next

--- .next/dist/pages/auth/index.js
+++ .next/dist/pages/auth/index.js
@@ -8,6 +8,10 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _User = require('../../components/User');
+
+var _User2 = _interopRequireDefault(_User);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = '/Users/billbuchanan/projects/react/tutorial/next-tutorial/pages/auth/index.js?entry';
@@ -17,15 +21,19 @@ var authIndexPage = function authIndexPage() {
   return _react2.default.createElement('div', {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 4
+      lineNumber: 5
     }
   }, _react2.default.createElement('h1', {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 5
+      lineNumber: 6
     }
-  }, 'The Auth Index Page'));
+  }, 'The Auth Index Page'), _react2.default.createElement(_User2.default, { name: 'Max', age: 28, __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }));
 };
 
 exports.default = authIndexPage;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2F1dGgvaW5kZXguanMiXSwibmFtZXMiOlsiUmVhY3QiLCJhdXRoSW5kZXhQYWdlIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPOzs7Ozs7Ozs7QUFFUCxJQUFNLGdCQUFnQixTQUFoQixBQUFnQixnQkFBQTt5QkFDcEIsY0FBQTs7Z0JBQUE7a0JBQUEsQUFDRTtBQURGO0FBQUEsR0FBQSxrQkFDRSxjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsS0FGa0IsQUFDcEIsQUFDRTtBQUZKLEFBTUE7O2tCQUFBLEFBQWUiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2JpbGxidWNoYW5hbi9wcm9qZWN0cy9yZWFjdC90dXRvcmlhbC9uZXh0LXR1dG9yaWFsIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2F1dGgvaW5kZXguanMiXSwibmFtZXMiOlsiUmVhY3QiLCJVc2VyIiwiYXV0aEluZGV4UGFnZSJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQU8sQUFBVTs7Ozs7Ozs7O0FBRWpCLElBQU0sZ0JBQWdCLFNBQWhCLEFBQWdCLGdCQUFBO3lCQUNwQixjQUFBOztnQkFBQTtrQkFBQSxBQUNFO0FBREY7QUFBQSxHQUFBLGtCQUNFLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxLQURGLEFBQ0UsQUFDQSx3Q0FBQSxBQUFDLGdDQUFLLE1BQU4sQUFBVyxPQUFNLEtBQWpCLEFBQXNCO2dCQUF0QjtrQkFIa0IsQUFDcEIsQUFFRTtBQUFBOztBQUhKLEFBT0E7O2tCQUFBLEFBQWUiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2JpbGxidWNoYW5hbi9wcm9qZWN0cy9yZWFjdC90dXRvcmlhbC9uZXh0LXR1dG9yaWFsIn0=
