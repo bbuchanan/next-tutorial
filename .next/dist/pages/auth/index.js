5403cc418a56a8df87789f3c78988e83eec6758f
Bill Buchanan <bbuchanan@gmail.com>
2018-01-10 16:04:26 -0500

    initial app setup with an index and auth/index.js page

---
+++ .next/dist/pages/auth/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/billbuchanan/projects/react/tutorial/next-tutorial/pages/auth/index.js?entry';
+
+
+var authIndexPage = function authIndexPage() {
+  return _react2.default.createElement('div', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  }, _react2.default.createElement('h1', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, 'The Auth Index Page'));
+};
+
+exports.default = authIndexPage;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2F1dGgvaW5kZXguanMiXSwibmFtZXMiOlsiUmVhY3QiLCJhdXRoSW5kZXhQYWdlIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPOzs7Ozs7Ozs7QUFFUCxJQUFNLGdCQUFnQixTQUFoQixBQUFnQixnQkFBQTt5QkFDcEIsY0FBQTs7Z0JBQUE7a0JBQUEsQUFDRTtBQURGO0FBQUEsR0FBQSxrQkFDRSxjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsS0FGa0IsQUFDcEIsQUFDRTtBQUZKLEFBTUE7O2tCQUFBLEFBQWUiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2JpbGxidWNoYW5hbi9wcm9qZWN0cy9yZWFjdC90dXRvcmlhbC9uZXh0LXR1dG9yaWFsIn0=
---
+++ .next/dist/pages/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/billbuchanan/projects/react/tutorial/next-tutorial/pages/index.js?entry';
+
+
+var indexPage = function indexPage() {
+  return _react2.default.createElement('div', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  }, _react2.default.createElement('h1', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, 'The Main Page'));
+};
+
+exports.default = indexPage;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiaW5kZXhQYWdlIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPOzs7Ozs7Ozs7QUFFUCxJQUFNLFlBQVksU0FBWixBQUFZLFlBQUE7eUJBQ2hCLGNBQUE7O2dCQUFBO2tCQUFBLEFBQ0U7QUFERjtBQUFBLEdBQUEsa0JBQ0UsY0FBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEtBRmMsQUFDaEIsQUFDRTtBQUZKLEFBTUE7O2tCQUFBLEFBQWUiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2JpbGxidWNoYW5hbi9wcm9qZWN0cy9yZWFjdC90dXRvcmlhbC9uZXh0LXR1dG9yaWFsIn0=
