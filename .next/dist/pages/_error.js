f80bb1287fdf07f21ba322935af203fb82c671b7
Bill Buchanan <bbuchanan@gmail.com>
2018-01-10 16:19:27 -0500

    basic 404 error handling via _error.js

--- .next/dist/pages/_error.js
+++ .next/dist/pages/_error.js
@@ -1,4 +1,49 @@
 'use strict';
 
-module.exports = require('next/dist/lib/error.js');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm5vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvcGFnZXMvX2Vycm9yLmpzIl0sIm5hbWVzIjpbIm1vZHVsZSIsImV4cG9ydHMiLCJyZXF1aXJlIl0sIm1hcHBpbmdzIjoiOztBQUFBLE9BQU8sQUFBUCxVQUFpQixBQUFqQiIsImZpbGUiOiJfZXJyb3IuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2JpbGxidWNoYW5hbi9wcm9qZWN0cy9yZWFjdC90dXRvcmlhbC9uZXh0LXR1dG9yaWFsIn0=
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _link = require('next/dist/lib/link.js');
+
+var _link2 = _interopRequireDefault(_link);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/billbuchanan/projects/react/tutorial/next-tutorial/pages/_error.js?entry';
+
+
+var errorPage = function errorPage() {
+  return _react2.default.createElement('div', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, _react2.default.createElement('h1', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }, 'Oops, something went wrong'), _react2.default.createElement('p', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }, 'Try ', _react2.default.createElement(_link2.default, { href: '/auth', __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }, _react2.default.createElement('a', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }, 'going back'))));
+};
+
+exports.default = errorPage;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL19lcnJvci5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkxpbmsiLCJlcnJvclBhZ2UiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFPOzs7Ozs7Ozs7QUFFUCxJQUFNLFlBQVksU0FBWixBQUFZLFlBQUE7eUJBQ2hCLGNBQUE7O2dCQUFBO2tCQUFBLEFBQ0U7QUFERjtBQUFBLEdBQUEsa0JBQ0UsY0FBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEtBREYsQUFDRSxBQUNBLCtDQUFBLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxLQUFPLHdCQUFBLEFBQUMsZ0NBQUssTUFBTixBQUFXO2dCQUFYO2tCQUFBLEFBQW1CO0FBQW5CO3FCQUFtQixjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsS0FIWixBQUNoQixBQUVFLEFBQU8sQUFBbUI7QUFIOUIsQUFPQTs7a0JBQUEsQUFBZSIsImZpbGUiOiJfZXJyb3IuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2JpbGxidWNoYW5hbi9wcm9qZWN0cy9yZWFjdC90dXRvcmlhbC9uZXh0LXR1dG9yaWFsIn0=
